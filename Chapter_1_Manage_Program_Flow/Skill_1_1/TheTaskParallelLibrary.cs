0f19fb17ff73b527902622bd94d878738ac079f8
zeyu.wang <zeyu.wang@medicaldirector.com>
2019-08-05 10:48:09 +1000

    Add tests for parallel invoke

--- Chapter_1_Manage_Program_Flow/Skill_1_1/TheTaskParallelLibrary.cs
+++ Chapter_1_Manage_Program_Flow/Skill_1_1/TheTaskParallelLibrary.cs
@@ -1,23 +1,46 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Shouldly;
 
 namespace Chapter_1_Manage_Program_Flow.Skill_1_1
 {
     public class TheTaskParallelLibrary
     {
+        public string SampleContentForParallelInvoke = "";
         [SetUp]
         public void Setup()
         {
         }
-
+        
+        //Test for Parallel Invoke
         [Test]
         public void CodeForParallelInvoke()
         {
-            Assert.Pass();
+            Parallel.Invoke(TaskForParallelInvoke_1, TaskForParallelInvoke_2);
+            SampleContentForParallelInvoke.ShouldBeOneOf(new string[]
+            {
+                "1342",
+                "3142"
+            });
         }
 
-        private void Task1()
+        private void TaskForParallelInvoke_1()
         {
-
+            SampleContentForParallelInvoke += "1";
+            Thread.Sleep(2000);
+            SampleContentForParallelInvoke += "2";
         }
+
+        private void TaskForParallelInvoke_2()
+        {
+            SampleContentForParallelInvoke += "3";
+            Thread.Sleep(1000);
+            SampleContentForParallelInvoke += "4";
+        }
+
+
     }
 }
