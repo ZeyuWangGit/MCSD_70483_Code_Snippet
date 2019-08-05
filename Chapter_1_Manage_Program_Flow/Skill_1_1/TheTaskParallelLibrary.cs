3e2ea2283f628906d2871ea5010460d138c5a063
zeyu.wang <zeyu.wang@medicaldirector.com>
2019-08-05 10:19:59 +1000

    Remove sample code

---
+++ Chapter_1_Manage_Program_Flow/Skill_1_1/TheTaskParallelLibrary.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Chapter_1_Manage_Program_Flow.Skill_1_1
+{
+    public class TheTaskParallelLibrary
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CodeForParallelInvoke()
+        {
+            Assert.Pass();
+        }
+
+        private void Task1()
+        {
+
+        }
+    }
+}
