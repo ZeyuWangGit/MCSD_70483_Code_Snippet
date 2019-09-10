44723c62c2df163494437da93f846b27d34205ed
Zeyu Wang <wzy.franklin@gmail.com>
2019-09-10 21:53:55 +1000

    Finish skill 1.1

---
+++ Chapter_1_Manage_Program_Flow/Skill_1_1/ConcurrentCollections.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Chapter_1_Manage_Program_Flow.Skill_1_1
+{
+    public class ConcurrentCollections
+    {
+        [Test]
+        public void SampleForBlockingCollection()
+        {
+            BlockingCollection<int> data = new BlockingCollection<int>(5);
+            Task.Run(() =>
+            {
+                // attempt to add 10 items to the collection - blocks after 5th
+                for (int i = 0; i < 11; i++)
+                {
+                    data.Add(i);
+                    Console.WriteLine("Data {0} added sucessfully.", i);
+                }
+                // indicate we have no more to add
+                data.CompleteAdding();
+            });
+            Console.WriteLine("Reading collection");
+            Task.Run(() =>
+            {
+                while (!data.IsCompleted)
+                {
+                    try
+                    {
+                        int v = data.Take();
+                        Console.WriteLine("Data {0} taken sucessfully.", v);
+                    }
+                    catch (InvalidOperationException) { }
+                }
+            });
+        }
+
+        [Test]
+        public void SampleForConcurrentQueue()
+        {
+            ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+            queue.Enqueue("Rob");
+            queue.Enqueue("Miles");
+            if (queue.TryPeek(out var str))
+                Console.WriteLine("Peek: {0}", str);
+            if (queue.TryDequeue(out str))
+                Console.WriteLine("Dequeue: {0}", str);
+        }
+
+        [Test]
+        public void SampleForConcurrentStack()
+        {
+            ConcurrentStack<string> stack = new ConcurrentStack<string>();
+            stack.Push("Rob");
+            stack.Push("Miles");
+            if (stack.TryPeek(out var str))
+                Console.WriteLine("Peek: {0}", str);
+            if (stack.TryPop(out str))
+                Console.WriteLine("Pop: {0}", str);
+        }
+
+        [Test]
+        public void SampleForConcurrentBag()
+        {
+            ConcurrentBag<string> bag = new ConcurrentBag<string> {"Rob", "Miles", "Hull"};
+            if (bag.TryPeek(out var str))
+                Console.WriteLine("Peek: {0}", str);
+            if (bag.TryTake(out str))
+                Console.WriteLine("Take: {0}", str);
+        }
+
+        [Test]
+        public void SampleForConcurrentDictionary()
+        {
+            ConcurrentDictionary<string, int> ages = new ConcurrentDictionary<string, int>();
+            if (ages.TryAdd("Rob", 21))
+                Console.WriteLine("Rob added successfully.");
+            Console.WriteLine("Rob's age: {0}", ages["Rob"]);
+            // Set Rob's age to 22 if it is 21
+            if (ages.TryUpdate("Rob", 22, 21))
+                Console.WriteLine("Age updated successfully");
+            Console.WriteLine("Rob's new age: {0}", ages["Rob"]);
+            // Increment Rob's age atomically using factory method
+            Console.WriteLine("Rob's age updated to: {0}",
+                ages.AddOrUpdate("Rob", 1, (name, age) => age = age + 1));
+            Console.WriteLine("Rob's new age: {0}", ages["Rob"]);
+        }
+    }
+}
--- Chapter_1_Manage_Program_Flow/Skill_1_1/ThreadsAndThreadPool.cs
+++ Chapter_1_Manage_Program_Flow/Skill_1_1/ThreadsAndThreadPool.cs
@@ -8,6 +8,12 @@ namespace Chapter_1_Manage_Program_Flow.Skill_1_1
 {
     public class ThreadsAndThreadPool
     {
+        static bool tickRunning;
+        public static ThreadLocal<Random> RandomGenerator =
+            new ThreadLocal<Random>(() =>
+            {
+                return new Random(2);
+            });
         [Test]
         public void CreateAThread()
         {
@@ -31,6 +37,62 @@ namespace Chapter_1_Manage_Program_Flow.Skill_1_1
         {
             Thread thread = new Thread(WorkOnData);
             thread.Start(99);
+        }
+
+        [Test]
+        public void AbortAThread()
+        {
+
+            tickRunning = true;
+            Thread tickThread = new Thread(() =>
+            {
+                while (tickRunning)
+                {
+                    Console.WriteLine("Tick");
+                    Thread.Sleep(1000);
+                }
+            });
+            tickThread.Start();
+            tickRunning = false;
+        }
+
+        [Test]
+        public void ThreadLocal()
+        {
+            Thread t1 = new Thread(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("t1: {0}", RandomGenerator.Value.Next(10));
+                    Thread.Sleep(500);
+                }
+            });
+            Thread t2 = new Thread(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("t2: {0}", RandomGenerator.Value.Next(10));
+                    Thread.Sleep(500);
+                }
+            });
+            t1.Start();
+            t2.Start();
+        }
+
+        [Test]
+        public void ThreadExecutionContext()
+        {
+            DisplayThread(Thread.CurrentThread);
+        }
+
+        public void DisplayThread(Thread t)
+        {
+            Console.WriteLine("Name: {0}", t.Name);
+            Console.WriteLine("Culture: {0}", t.CurrentCulture);
+            Console.WriteLine("Priority: {0}", t.Priority);
+            Console.WriteLine("Context: {0}", t.ExecutionContext);
+            Console.WriteLine("IsBackground?: {0}", t.IsBackground);
+            Console.WriteLine("IsPool?: {0}", t.IsThreadPoolThread);
         }
 
         public void ThreadHello()
